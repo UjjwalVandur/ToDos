f1ba3c087c4860c698895c944b118d94559d615e
UjjwalVandur <ujjwalvandur@gmail.com>
2024-08-17 20:28:01 +0530

    Resolved merge conflicts

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -108,8 +108,8 @@ const Home = () => {
                     <div className={item.isCompleted ? "line-through" : ""}>{item.todo}</div>
                   </div>
                   <div className="buttons flex h-full">
-                    <button onClick={(e) => { handleEdit(e, item.id) }} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md m-1'><FaEdit /></button>
-                    <button onClick={(e) => { handleDelete(e, item.id) }} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md m-1'><MdDeleteForever /></button>
+                    <button onClick={(e) => handleEdit(e, item.id) } className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md m-1'><FaEdit /></button>
+                    <button onClick={(e) => handleDelete(e, item.id) } className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md m-1'><MdDeleteForever /></button>
                   </div>
                 </div>
               })}
